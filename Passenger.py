0225f014710b31b779bffb1c25483b2ab8a008f1
ldt20 <Ldt20020517@bjfu.edu.cn>
2025-01-10 20:53:07 +0800

    增加标志位，在统计乘客信息的时候起作用，只在train的时候统计乘客的信息

--- Passenger.py
+++ Passenger.py
@@ -1,8 +1,6 @@
 """
 用于编写乘客类的文件
 """
-import pandas as pd
-import numpy as np
 
 
 class Passenger:
@@ -14,7 +12,7 @@ class Passenger:
     age, sibSp, parch, embarked = {}, {}, {}, {}
 
     def __init__(self, passengerId: int, survived: int, pClass: int,
-                 sex: int, age: int | str, sibSp: int, parch: int, embarked: str):
+                 sex: int, age: int | str, sibSp: int, parch: int, embarked: str, trainOrNot: bool = True):
         """
         初始化乘客类
         :param passengerId: 乘客ID
@@ -25,6 +23,7 @@ class Passenger:
         :param sibSp: 同行兄弟/配偶数
         :param parch: 同行父母/子女数
         :param embarked: 登船港口 C/Q/S
+        :param trainOrNot: 是否为训练集，默认为True
         """
         self.passengerId = passengerId
         self.survived = survived
@@ -34,23 +33,38 @@ class Passenger:
         self.sibSp = sibSp
         self.parch = parch
         self.embarked = embarked
+        if trainOrNot:
+            self.__statisticsInformation()  # 统计乘客信息
+        else:
+            pass
 
-    def _changeAge(self, age):
+    @staticmethod
+    def _changeAge(age):
         """
-        建议只在类内部调用
+        建议只在类内部调用，设置为静态方法
         将年龄转换为年龄类别
         :param age: 初始化时传入的年龄
         :return: 转换完成之后的年龄类别
         """
-        age_class = -1
         if age == "missing":
-            return age_class
-        if age <= 20:
-            age_class = 0
-        elif age <= 40:
-            age_class = 1
-        elif age <= 60:
-            age_class = 2
+            return -1
         else:
-            age_class = 3
-        return age_class
+            return age // 20 if age < 60 else 3
+
+    def __statisticsInformation(self):
+        """
+        统计乘客信息，填充类的静态变量，利于贝叶斯模型的计算
+        私有方法，只建议在类内部调用
+        :return: 无返回值
+        """
+        Passenger.survived[self.survived] = 1 if Passenger.survived.get(self.survived) is None else Passenger.survived[self.survived] + 1
+        Passenger.pClass[self.pClass] = 1 if Passenger.pClass.get(self.pClass) is None else Passenger.pClass[self.pClass] + 1
+        Passenger.sex[self.sex] = 1 if Passenger.sex.get(self.sex) is None else Passenger.sex[self.sex] + 1
+        Passenger.age[self.age] = 1 if Passenger.age.get(self.age) is None else Passenger.age[self.age] + 1
+        Passenger.sibSp[self.sibSp] = 1 if Passenger.sibSp.get(self.sibSp) is None else Passenger.sibSp[self.sibSp] + 1
+        Passenger.parch[self.parch] = 1 if Passenger.parch.get(self.parch) is None else Passenger.parch[self.parch] + 1
+        Passenger.embarked[self.embarked] = 1 if Passenger.embarked.get(self.embarked) is None else Passenger.embarked[self.embarked] + 1
+
+
+if __name__ == "__main__":
+    pass
